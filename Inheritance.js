27e14d453d046246168a5953035420c1b2ff83e9
Muhammad Adil Memon <96164867+kaziadilmemon@users.noreply.github.com>
2023-05-03 23:01:03 +0500

    Update Inheritance.js

--- Inheritance.js
+++ Inheritance.js
@@ -1,1 +1,31 @@
+/* Inheritance is a way for one class to extend another class.This is done by using extends keyword. The extends keyword is used to extend another class. */
 
+class Animal {
+  constructor(name, color) {
+    this.name = name
+    this.color = color
+  }
+  run() {
+    console.log(this.name + " is running!")
+  }
+  shout() {
+    console.log(this.name + " is barking!")
+  }
+}
+
+class Monkey extends Animal {
+  eatBanana() {
+    console.log(this.name + " is eating banana")
+  }
+  hide() {
+    console.log(`${this.name} is hiding`)
+  }
+}
+
+let ani = new Animal("Bruno", "white")
+let m = new Monkey("Chimpu", "orange")
+
+ani.shout()
+m.eatBanana()
+m.hide()
+// ani.hide() //This will throw an error
